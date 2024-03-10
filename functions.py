43742e7f3b4940fde8a139e3e10689a82a94f3db
Maxim Talancev <maxtalwork@gmail.com>
2024-03-10 17:28:27 +0300

    Добавили функции для сортировки (статус и дата)

--- functions.py
+++ functions.py
@@ -1,10 +1,21 @@
 import json
 
+
 def load_operations():
     """
     Загружает список операций из файла
     """
     with open('operations.json', 'r', encoding='utf-8') as f:
-        file = f.read()
-        operations = json.loads(file)
-    return operations
+        return json.load(f)
+
+
+def is_executed(operation):
+    return operation.get('state') == 'EXECUTED'
+
+
+def date_order(operation):
+    return operation.get('date')
+
+
+def print_operation(operation):
+    return f"{operation.get('state')} {operation.get('date')[:10]} **{operation.get('from', '*********')[5:6]}"
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from functions import load_operations
+from functions import (load_operations, is_executed, date_order, print_operation)
 
-loaded_operationd = load_operations()
+operations = load_operations()
+print(operations)
+
+operation = list(
+    map(
+        print_operation,
+        filter(
+            is_executed,
+            sorted(
+                filter(
+                    lambda x: x.get('date') is not None, operations),
+                key=date_order))))[:5]
+
+
+print("\n".join(operation))
